dd7f84f83c9c6e55b5065fef1789a2436c804778
Lora <jonathan.lora.95@gmail.com>
2018-09-08 16:04:22 -0400

    created many-to-many relation between users and assets

--- crypto-client/src/components/Asset.jsx
+++ crypto-client/src/components/Asset.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import Block from './Block';
 
-function doSomething(arr) {
-  const result = arr.reduce((accumulator, value) => {
-    return accumulator + value;
-  }, 0);
-  return (
-    <div>
-    {result}
-    </div>
-  )
-}
-
 function Asset({ asset }) {
   return (
     <div>
       <h1>{asset.name}</h1>
       <span>
       {
+        // asset.blocks.map(block => (
+        //   <div key={block.block_height}>
+        //     <Block block={block} />
+        //   </div>
+        // ))
         asset.blocks.reduce((total, block) => total + block.transaction_num, 0)
       }
       </span>
--- crypto-client/src/components/Block.jsx
+++ crypto-client/src/components/Block.jsx
@@ -1,30 +1,5 @@
 import React from 'react';
 
-// function doSomething(num) {
-//   num.block.
-// }
-
-function doSomething(num) {
-  return num + 1;
-}
-
-// function doSomething(num) {
-//   const arr = [];
-//   debugger;
-//   arr.push(num);
-//   debugger;
-//   return arr;
-// }
-
-// function doSomething(num) {
-//   const arr = [];
-//   arr.push(num);
-//   const value = arr.reduce((accumulator, value) => {
-//     return accumulator + value;
-//   }, 0)
-//   return value;
-// }
-
 function Block({ block }) {
   return (
     <span>{block.transaction_num}</span>
