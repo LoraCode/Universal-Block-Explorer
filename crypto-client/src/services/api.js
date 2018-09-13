902ff7e0b84cff4207d69edcb329becafd211538
Lora <jonathan.lora.95@gmail.com>
2018-09-13 18:41:04 -0400

    w.e

--- crypto-client/src/services/api.js
+++ crypto-client/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3000';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const BASE_URL = 'http://localhost:3000'
 
 const fetchAssets = async () => {
   try {
